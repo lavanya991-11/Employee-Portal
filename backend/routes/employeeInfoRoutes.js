a6761867be7c94709b66d79fec1116c5301afd09
Lavanya <Lavanya@novasoft.global>
2026-06-28 16:25:03 +0530

    Revert "Add employee identification-type update API (BC PATCH)"
    This reverts commit feec4d8a4a05f0b5c58cac8c319817e5154bb3ae.

--- backend/routes/employeeInfoRoutes.js
+++ backend/routes/employeeInfoRoutes.js
@@ -4,15 +4,12 @@ const { protect } = require('../middleware/authmiddleware');
 const {
     getMyInfo,
     saveMyInfo,
-    getAllInfo,
-    updateIdentification
+    getAllInfo
 } = require('../controllers/employeeInfoController');
 
 router.get('/my', protect, getMyInfo);
 router.post('/my', protect, saveMyInfo);
 router.put('/my', protect, saveMyInfo);
 router.get('/all', protect, getAllInfo);
-router.put('/identification', protect, updateIdentification);
-router.post('/identification', protect, updateIdentification);
 
 module.exports = router;
